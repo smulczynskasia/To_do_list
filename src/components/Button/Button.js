1f0a3b0419a33950033989baf56ee0a076d7ed4b
smulczynskasia <smulczynska.katarzyna@gmail.com>
2022-09-11 11:14:24 +0000

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,22 +1,11 @@
 import styles from './Button.module.scss';
-<<<<<<< HEAD
 
 const Button = props => {
     return (
         <button className={styles.button}>
             {props.children}
-=======
-const Button = props => {
-    return (
-        <button className={styles.button}>
-            {props.text}
->>>>>>> refs/remotes/origin/main
         </button>
     );
 }
 
-<<<<<<< HEAD
 export default Button;
-=======
-export default Button;
->>>>>>> refs/remotes/origin/main
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,15 +7,13 @@ const SearchForm = () => {
         <form className={styles.searchForm}>
             <TextInput placeholder="Search..." />
             <TextInput placeholder="Add new column" />
-<<<<<<< HEAD
+
             <Button>
                 <span className="fa fa-search" />
             </Button>
             <Button>Create</Button>
-=======
             <Button text="Search" />
             <Button text="Create" />
->>>>>>> refs/remotes/origin/main
         </form>
     );
   };
